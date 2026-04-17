1f8d5606fbdac8ec849a6a7cdd2c68185dc4e5d1
Marconny Marques <130397974+Marconny-Marques@users.noreply.github.com>
2026-04-17 10:37:08 -0300

    Create Produto.java

---
+++ src/Produto.java
@@ -0,0 +1,51 @@
+
+public class Produto{
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidadeEmEstoque;
+    
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDecricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+
+    
+
+    
+
+    
+}
